3a4ce49a87b6a13bce4304d5527217f158b1faa3
Romain-Constant <romain.constant59@gmail.com>
2023-11-16 16:05:59 +0100

    :card_file_box:added cascade for deleting posts when deleting user, added 3 users in db script and changed comments to english

--- backend/database.sql
+++ backend/database.sql
@@ -1,20 +1,20 @@
--- Création du schéma
+-- Create the schema
 CREATE SCHEMA IF NOT EXISTS `ts_wild_wall`;
 
--- Création de la table "role"
+-- Create the "role" table
 CREATE TABLE IF NOT EXISTS `ts_wild_wall`.`role` (
   `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `role_name` VARCHAR(45) NOT NULL,
   `role_code` VARCHAR(45) NOT NULL
 );
 
--- Création de la table "statut"
+-- Create the "statut" table
 CREATE TABLE IF NOT EXISTS `ts_wild_wall`.`statut` (
   `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `statut_name` VARCHAR(45) NOT NULL
 );
 
--- Création de la table "user" avec une clé étrangère "role_id" et une contrainte d'unicité sur le champ "username"
+-- Create the "user" table with a foreign key "role_id" and a unique constraint on the "username" field
 CREATE TABLE IF NOT EXISTS `ts_wild_wall`.`user` (
   `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `username` VARCHAR(45) NOT NULL UNIQUE,
@@ -23,14 +23,14 @@ CREATE TABLE IF NOT EXISTS `ts_wild_wall`.`user` (
   FOREIGN KEY (`role_id`) REFERENCES `ts_wild_wall`.`role`(`id`)
 );
 
--- Création de la table "color"
+-- Create the "color" table
 CREATE TABLE IF NOT EXISTS `ts_wild_wall`.`color` (
   `id` INT NOT NULL PRIMARY KEY,
   `color_name` VARCHAR(45) NOT NULL,
   `color_code` VARCHAR(45) NOT NULL
 );
 
--- Création de la table "post" avec des clés étrangères "statut_id", "user_id" et "color_id"
+-- Create the "post" table with foreign keys "statut_id", "user_id", and "color_id" and ON DELETE CASCADE
 CREATE TABLE IF NOT EXISTS `ts_wild_wall`.`post` (
   `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `post_text` VARCHAR(255) NOT NULL,
@@ -39,32 +39,39 @@ CREATE TABLE IF NOT EXISTS `ts_wild_wall`.`post` (
   `user_id` INT NOT NULL,
   `color_id` INT NOT NULL,
   FOREIGN KEY (`statut_id`) REFERENCES `ts_wild_wall`.`statut`(`id`),
-  FOREIGN KEY (`user_id`) REFERENCES `ts_wild_wall`.`user`(`id`),
+  FOREIGN KEY (`user_id`) REFERENCES `ts_wild_wall`.`user`(`id`) ON DELETE CASCADE,
   FOREIGN KEY (`color_id`) REFERENCES `ts_wild_wall`.`color`(`id`)
 );
 
--- Insertion de données dans la table "role"
+-- Insert data into the "role" table
 INSERT IGNORE INTO `ts_wild_wall`.`role` (`id`, `role_name`, `role_code`)
 VALUES ('1', 'admin', '2013'),
        ('2', 'delegate', '4004'),
        ('3', 'wilder', '5067');
 
--- Insertion de données dans la table "statut"
+-- Insert data into the "statut" table
 INSERT IGNORE INTO `ts_wild_wall`.`statut` (`id`, `statut_name`)
 VALUES ('1', 'main_wall'),
        ('2', 'archived');
 
--- Insertion de données dans la table "color"
+-- Insert data into the "color" table
 INSERT IGNORE INTO `ts_wild_wall`.`color` (`id`, `color_name`, `color_code`)
 VALUES ('1', 'green', '#c7ebb3'),
        ('2', 'pink', '#ffd5f8'),
        ('3', 'blue', '#c5e8f1'),
        ('4', 'yellow', '#f8eaae');
 
--- Insertion de l'utilisateur "Romain" avec le mot de passe "Romain" et le rôle "admin"
+-- Insert the user "Romain" with the password "Romain" and the role "admin"
 INSERT INTO `ts_wild_wall`.`user` (`username`, `password`, `role_id`)
 VALUES ('Romain', '$2b$10$tz2Kn/x6zFIxy77Aq6bQQedNcbGoJNoJO1g4RGfTiOeAfflYCwTuO', 1);
 
--- Insertion du post "Premier post" avec la couleur bleue à la date du jour
+-- Insert three users with the role "wilder"
+INSERT INTO `ts_wild_wall`.`user` (`username`, `password`, `role_id`)
+VALUES 
+  ('User1', '$2b$10$tz2Kn/x6zFIxy77Aq6bQQedNcbGoJNoJO1g4RGfTiOeAfflYCwTuO', 3),
+  ('User2', '$2b$10$tz2Kn/x6zFIxy77Aq6bQQedNcbGoJNoJO1g4RGfTiOeAfflYCwTuO', 3),
+  ('User3', '$2b$10$tz2Kn/x6zFIxy77Aq6bQQedNcbGoJNoJO1g4RGfTiOeAfflYCwTuO', 3);
+
+-- Insert the post "First post" with the blue color on the current date
 INSERT INTO `ts_wild_wall`.`post` (`post_text`, `post_date`, `statut_id`, `user_id`, `color_id`)
-VALUES ('Premier post', NOW(), 1, 1, 3);
+VALUES ('First post', NOW(), 1, 1, 3);
